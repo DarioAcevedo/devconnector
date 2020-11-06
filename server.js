0558559d190ee8857784cdfac30e04c7a7102d10
DarioAcevedo <47285245+DarioAcevedo@users.noreply.github.com>
2020-11-05 21:58:56 -0600

    Agregada conexión con base de datos Agregado también rutas de usuarios, perfiles, autenticación y posts

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get("/", () => "API Running");
+//Connect to database
+connectDB();
+
+//Define routes
+
+app.use('/api/users',require('./routes/api/users'));
+app.use('/api/auth',require('./routes/api/auth'));
+app.use('/api/profile',require('./routes/api/profile'));
+app.use('/api/posts',require('./routes/api/posts'));
+
+app.get("/", (req, res) => res.send("API Running"));
 
 //Define the port that is used. Default 5000
 const PORT = process.env.PORT || 5000;
